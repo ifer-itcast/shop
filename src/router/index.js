9d0eb4f0d6520a155f2a26876362691bd6bb5d18
ifer-itcast <yangkangkang@itcast.cn>
2020-09-12 21:49:26 +0800

    Home组件简单处理

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
 
 Vue.use(VueRouter)
 
@@ -14,7 +15,14 @@ const router = new VueRouter({
     component: Login
   }, {
     path: '/home',
-    component: Home
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: Welcome
+      }
+    ]
   }]
 })
 
