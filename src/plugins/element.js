1851410e64602103e7a2887ffaf06c9eb54f27b6
ifer-itcast <yangkangkang@itcast.cn>
2020-09-13 00:41:09 +0800

    用户的增删改查

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,30 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Dialog,
+  Tooltip,
+  Pagination,
+  Switch,
+  MessageBox
+} from 'element-ui'
 
 Vue.use(Form)
 Vue.use(FormItem)
@@ -12,4 +37,16 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Dialog)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Switch)
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
