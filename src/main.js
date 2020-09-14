61763d0d5cd2c1cdb20c041a44ec25e3d052e2b9
ifer-itcast <yangkangkang@itcast.cn>
2020-09-14 14:48:21 +0800

    商品分类

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 // 全局样式
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
 // 挂载 axios 到 Vue 原型
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -15,6 +16,8 @@ axios.interceptors.request.use(config => {
 })
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
