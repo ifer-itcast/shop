664f014181f3957047dee2170f8208640b32d85f
ifer-itcast <yangkangkang@itcast.cn>
2020-09-14 11:33:29 +0800

    角色管理

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -24,7 +24,10 @@ import {
   Pagination,
   Switch,
   MessageBox,
-  Tag
+  Tag,
+  Tree,
+  Select,
+  Option
 } from 'element-ui'
 
 Vue.use(Form)
@@ -50,5 +53,8 @@ Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Switch)
 Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 
 Vue.use(VueRouter)
 
@@ -31,6 +32,10 @@ const router = new VueRouter({
       {
         path: '/rights',
         component: Rights
+      },
+      {
+        path: '/roles',
+        component: Roles
       }
     ]
   }]
